f9579ed765f5328f4daecc17cc1c04fa38cc9f62
Mark Linn <marktlinn@gmail.com>
2022-10-27 12:19:04 +0200

    add ideas file

---
+++ ideas.js
@@ -0,0 +1,30 @@
+Hi // Testing
+
+// User stories:
+// User can see the time interval words must be typed in displayed in the app window.
+    // 1 minute? countdown?
+
+// User can see the number of successful attempts and the number of total attempts in a score box.
+    // Error count: ; correct count: 
+
+// User can click a 'Start Practice' button to start the practice session.
+
+// User can see the prompt word displayed in a text box.
+    // one sentence at a time
+
+// User can begin typing the word in a text input box.
+
+// User can see the letters that have been typed flash if an incorrect letter is entered and the text input box will be cleared.
+    // Flag errors in text input
+
+// User can see the message adjacent to the text input box indicating the user should try again if an incorrect letter is entered.
+    // block user input until the error is corrected.
+
+// User can see a congratulations message if the word is correctly typed.
+    // correct words are green to show it is correct; red if erroneous
+
+// User can see the time interval words must be typed decremented by a small amount if the word is correctly typed. => 300 words, 299, 298 ['word', 'word'].length = 300; 299
+
+// User can click a 'Stop Practice' button to stop the practice session.
+    //  stop the count on click
+    // stop input
